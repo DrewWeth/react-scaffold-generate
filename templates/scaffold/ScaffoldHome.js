0f49429717f2f70d205a64c9cca916a719b6b7c4
Drew Wetherington <dwetherington@wellprincipled.com>
2021-01-13 18:12:22 -0600

    Home update, table wrappers

--- templates/scaffold/ScaffoldHome.js
+++ templates/scaffold/ScaffoldHome.js
@@ -2,23 +2,35 @@ import React from 'react';
 
 const table = [
     ["Scaffold documentation", "https://github.com/DrewWeth/react-scaffold-generate"],
-    ["Demo App", "http://rsg.drewweth.com/"]
+    ["Demo App", "http://rsg.drewweth.com/"],
+    ["NPM Package", "https://www.npmjs.com/package/react-scaffold-generate"]
 ]
 
 
 const ScaffoldHome = () => {
     return <div>
+        <h3>About</h3>
+        <div className="section">
+            <p>
+                This app was made with 3 commands, and one was command was <code>cd</code>.
+                <div className="codeExample"> 
+                    <img className="responsive" src="https://github.com/DrewWeth/react-scaffold-generate/blob/main/static/generate-no-padding.png?raw=true"></img>
+                </div>
+            </p>
+        </div>
         <h3>Links</h3>
-        <table className="styled-table">
-            <tbody>
-                {table.map( ([name, link]) =>{
-                    return <tr>
-                        <td>{name}</td>
-                        <td><a className="navLink" href={link} target="_blank">{link}</a></td>
-                    </tr>
-                })}
-            </tbody>
-        </table>
+        <div className="styled-table-container">
+            <table className="styled-table">
+                <tbody>
+                    {table.map( ([name, link]) =>{
+                        return <tr>
+                            <td>{name}</td>
+                            <td><a className="navLink" href={link} target="_blank">{link}</a></td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
         <div className="spacerTop40">
             <p>Thank you for checking out this project!</p>
             <a href="https://www.buymeacoffee.com/drewweth"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=drewweth&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"/></a>
--- templates/scaffold/components/List.js
+++ templates/scaffold/components/List.js
@@ -18,30 +18,32 @@ function List () {
         <Link to='/{{componentName}}/new' className="navLink">New {{ComponentName}}</Link>
       </div>
       <div>
-        <table className="styled-table">
-          <thead>
-            <tr>
-              <th>ID</th>
-              {attrs.map(({name}, kIndex) =>{
-                return <th key={kIndex}>{name}</th>
-              })}
-              {elements.length > 0 && <th></th>}
-            </tr>
-          </thead>
-          <tbody>
-            {elements.map((element, eIndex) =>
-              <tr key={eIndex}>
-                <td><code>{element["_id"]}</code></td>
-                {attrs.map(( {name}, vIndex) => {
-                  return <td key={vIndex}><code>{element[name]}</code></td>
+        <div className="styled-table-container">
+          <table className="styled-table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                {attrs.map(({name}, kIndex) =>{
+                  return <th key={kIndex}>{name}</th>
                 })}
-                <td className='rightAlign'>
-                  <ActionButtons modelName={"{{componentName}}"} id={element["_id"]} flipRightSpace={true} showDetails showEdit />
-                </td>
+                {elements.length > 0 && <th></th>}
               </tr>
-            )}
-            </tbody>
-          </table>
+            </thead>
+            <tbody>
+              {elements.map((element, eIndex) =>
+                <tr key={eIndex}>
+                  <td><code>{element["_id"]}</code></td>
+                  {attrs.map(( {name}, vIndex) => {
+                    return <td key={vIndex}><code>{element[name]}</code></td>
+                  })}
+                  <td className='rightAlign'>
+                    <ActionButtons modelName={"{{componentName}}"} id={element["_id"]} flipRightSpace={true} showDetails showEdit />
+                  </td>
+                </tr>
+              )}
+              </tbody>
+            </table>
+          </div>
         </div>
     </div>
   );
